59542576b9af3b71a35b6035f34ee0b624aed118
Billazeezdinho <billazeezdinho@gmail.com>
2026-04-04 17:58:50 +0100

    feat: email transporter and setup

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,15 +1,41 @@
-import { transporter } from "../config/mail.js";
+const nodeMailer = require('nodemailer');
+const user = process.env.user;
+const pass = process.env.pass;
 
-export const sendEmail = async (data) => {
-  await transporter.sendMail({
-    from: `"Nico Farms" <${process.env.EMAIL_USER}>`,
-    to: "nicofarms@email.com",
-    subject: "New Contact Message",
-    text: `
-      Name: ${data.name}
-      Email: ${data.email}
-      Message: ${data.message}
-      Type: ${data.type}
-    `,
-  });
-};
+const sendMail = async ( options )=>{``
+  
+    const transporter = await nodeMailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        service: process.env.EMAIL_SERVICE,
+        port: 465,
+
+        secure: true, // true for port 465, false for other ports
+        auth: {
+          user: "obusco4lyfe@gmail.com",
+          pass: "nugn xilt pwbv arse"
+        },
+        // tls: {
+        //     rejectUnauthorized: false, // Bypass SSL verification
+        //   }        
+      });
+
+      async function main() {
+        // send mail with defined transport object
+        const info = await transporter.sendMail({
+          from: `"Fred Foo 👻" <${user}>`, // sender address
+          to: options.email, // list of receivers
+          subject: options.subject, // Subject line
+          html: options.html, // html body
+        });
+
+        console.log("Message sent: %s", info.messageId);
+      }
+
+
+      const mailOption = {
+        subject: options.subject, text:options.text, from:"obusco4lyfe@gmail.com", to: options.email, html:options.html
+      };
+      await transporter.sendMail(mailOption)
+
+}
+module.exports = sendMail;
